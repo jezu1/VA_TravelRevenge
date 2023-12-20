1c39b5735210309fd8a61ae69a3c27f8c85dee4e
Jezuela Gega <jezuela@pc-28.home>
2023-12-20 16:57:08 +0100

    some results

--- script.js
+++ script.js
@@ -1,171 +1,208 @@
-// Load data from the CSV file
-d3.csv("./data/tourism_exp_dest.csv", d => {
-    return {
 
-    Origin_Country_Code,Origin_Country,Destination_Country_Code,Destination_Country,Purpose,Duration,Expenditure_Type,Total_Expenditure_EUR,Avg_Exp_per_Trip_EUR,Avg_Exp_per_Night_EUR
-    year: +d.year,  
-    Origin_Country_Code: d.Origin_Country_Code,
-    Origin_Country_Name: d.Origin_Country_Name,
-    Destination_Country_Code: d.Destination_Country_Code,
-    Destination_Country_Name: d.Destination_Country_Name,
-    Purpose: d.Purpose,
-    Duration: d.Duration,
-    Expenditure_Type: d.Expenditure_Type,
-    Total_Expenditure_EUR: +d.Total_Expenditure_EUR,
-    Avg_Exp_per_Trip_EUR: +d.Avg_Exp_per_Trip_EUR,
-    Avg_Exp_per_Night_EUR: +d.Avg_Exp_per_Night_EUR
-    };
-    }).then(data => {
-    // Filter data based on selections
-    data = data.filter(d => (d.Year === "2019" || d.Year === "2022"));
-    
-    years = Array.from(new Set(data.map(d => d.Year)));
-    countries_o = Array.from(new Set(data.map(d => d.Origin_Country)));
-    countries_d = Array.from(new Set(data.map(d => d.Destination_Country)));
-    purposes = Array.from(new Set(data.map(d => d.Purpose)));
-    expenses = Array.from(new Set(data.map(d => d.Total_Expenditure_EUR)));
-  
+        // Load data from the CSV file
+        d3.csv("./data/tourism_exp_dest.csv").then(data => {
+            // Parse data
+                data.forEach(d => {
+                    d.Year = +d.Year; // Capital 'Y' for 'Year'
+                    d.Origin_Country_Code = d.Origin_Country_Code;
+                    d.Origin_Country_Name = d.Origin_Country; // 'Origin_Country' instead of 'Origin_Country_Name'
+                    d.Destination_Country_Code = d.Destination_Country_Code;
+                    d.Destination_Country_Name = d.Destination_Country; // 'Destination_Country' instead of 'Destination_Country_Name'
+                    d.Purpose = d.Purpose;
+                    d.Total_Expenditure_EUR = +d.Total_Expenditure_EUR;
+                });
+            const uniqueDestinationCountries = Array.from(new Set(data.map(d => d.Destination_Country_Name)));
+            const uniqueOriginCountries = Array.from(new Set(data.map(d => d.Origin_Country_Name)));
+                
+            const uniqueCountries = [...new Set([...uniqueDestinationCountries, ...uniqueOriginCountries])];
+            console.log(uniqueCountries);
 
-  createLineChart(expenses2020, expenses2022); 
+            // Create initial chart
+            createChart(uniqueCountries[0], uniqueCountries[1]); // Default country selection
 
-  // Update the charts when a change takes place to the DOM
-  d3.selectAll("#Origin_Country_Name, #Destination_Country_Name")
-      .on("change", updateCharts);
-    
-    // Calculate expenses for 2020 and 2022 within this function
-    const expenses2020 = data.filter(d => d.Year === "2019" || d.Year === "2020");
-    const expenses2022 = data.filter(d => d.Year === "2021" || d.Year === "2022");
-    mergedExpenses = expenses2020.map(expense => ({
-        Purpose: expense.Purpose,
-        x1: +expenses2020.find(e => e.Purpose === expense.Purpose)?.Total_Expenditure_EUR || 0,
-        x2: +expenses2022.find(e => e.Purpose === expense.Purpose)?.Total_Expenditure_EUR || 0
-    }));
-    console.log(mergedExpenses);
+            // Function to calculate expenses for 2019 and 2022
+            function calculateExpenses(selectedOriginCountry, selectedDestinationCountry) {
+                const filteredData = data.filter(d =>
+                    d.Origin_Country_Name === selectedOriginCountry &&
+                    d.Destination_Country_Name === selectedDestinationCountry &&
+                    (d.Year === 2019 || d.Year === 2022)
+                );
+          
+                const expenses2019 = filteredData.filter(d => d.Year === 2019).reduce((acc, cur) => {
+                    acc[cur.Purpose] = acc[cur.Purpose] ? acc[cur.Purpose] + cur.Total_Expenditure_EUR : cur.Total_Expenditure_EUR;
+                    return acc;
+                }, {});
+          
+                const expenses2022 = filteredData.filter(d => d.Year === 2022).reduce((acc, cur) => {
+                    acc[cur.Purpose] = acc[cur.Purpose] ? acc[cur.Purpose] + cur.Total_Expenditure_EUR : cur.Total_Expenditure_EUR;
+                    return acc;
+                }, {});
+                //debug
+                console.log(selectedOriginCountry);
+                console.log(selectedDestinationCountry);
+                console.log(expenses2019);
+                console.log(expenses2022);
 
-    
-    function createLineChart(expenses2020, expenses2022, mergedExpenses) {
+                return Object.keys(expenses2019).map(Purpose => ({
+                    Purpose: Purpose,
+                    x1: expenses2019[Purpose] || 0,
+                    x2: expenses2022[Purpose] || 0
+                
+                }))
+               
+            }
+            
 
-    // Create a Cleveland dot plot in d3.js
-    // https://www.d3-graph-gallery.com/graph/dotplot_cleveland.html
-    
-    // Remove previous chart
-    d3.select("#chart").selectAll("*").remove();
+            // Function to create the Cleveland dot plot
+            function createChart(selectedOriginCountry, selectedDestinationCountry) {
+                const expensesData = calculateExpenses(selectedOriginCountry, selectedDestinationCountry);
+            
+                // Select the chart container
+                      // Dimensions and margins for the chart
+                const margin = { top: 30, right: 250, bottom: 30, left: 120 };
+                const width = 1000 - margin.left - margin.right;
+                const height = 400 - margin.top - margin.bottom;
 
-      // Dimensions and margins for the chart
-    const margin = { top: 30, right: 250, bottom: 30, left: 120 };
-    const width = 1000 - margin.left - margin.right;
-    const height = 400 - margin.top - margin.bottom;
+            
+                // Remove any existing SVG to avoid duplication
+                d3.select("#chart").selectAll("svg").remove();
+            
+                // Create SVG container
+                const svg = d3.select("#chart")
+                    .append("svg")
+                    .attr("width", width + margin.left + margin.right)
+                    .attr("height", height + margin.top + margin.bottom)
+                    .append("g")
+                    .attr("transform", `translate(${margin.left},${margin.top})`);
 
-      // Create SVG container
-    const svg = d3.select("#chart")
-        .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", `translate(${margin.left},${margin.top})`);
+                // X axis scale
+                const xScale = d3.scaleLinear()
+                    .domain([0, d3.max(expensesData, d => Math.max(d.x1, d.x2))])
+                    .range([0, width]);
 
-      // X axis scale
-    const x = d3.scaleLinear()
-        .domain([0, d3.max(mergedExpenses, d => Math.max(d.x1, d.x2))])
-        .range([0, width]);
+                // Y axis scale
+                const yScale = d3.scaleBand()
+                    .domain(expensesData.map(d => d.Purpose))
+                    .range([0, height])
+                    .padding(0.1);
 
-      // Y axis scale
-    const y = d3.scaleBand()
-        .domain(mergedExpenses.map(d => d.Purpose))
-        .range([0, height])
-        .padding(0.1);
+                // Draw lines
+                svg.selectAll(".line")
+                    .data(expensesData)
+                    .enter()
+                    .append("line")
+                    .attr("class", "line")
+                    .attr("x1", d => xScale(d.x1)) // This should be "x1" for 2019
+                    .attr("y1", d => yScale(d.Purpose) + yScale.bandwidth() / 2)
+                    .attr("x2", d => xScale(d.x2)) // This should be "x2" for 2022
+                    .attr("y2", d => yScale(d.Purpose) + yScale.bandwidth() / 2)
+                    .attr("stroke", "GREY")
+                    .attr("stroke-width", 1);
 
-    // Draw lines
-    svg.selectAll(".line")
-        .data(mergedExpenses)
-        .enter()
-        .append("line")
-        .attr("class", "line")
-        .attr("x1", d => x(d.x1))
-        .attr("y1", d => y(d.Purpose) + y.bandwidth() / 2)
-        .attr("x2", d => x(d.x2))
-        .attr("y2", d => y(d.Purpose) + y.bandwidth() / 2)
-        .attr("stroke", "GREY")
-        .attr("stroke-width", 1);
+                // Draw dots for 2019
+                svg.selectAll(".dot2019")
+                    .data(expensesData)
+                    .enter()
+                    .append("circle")
+                    .attr("class", "dot2019")
+                    .attr("cx", d => xScale(d.x1))
+                    .attr("cy", d => yScale(d.Purpose) + yScale.bandwidth() / 2)
+                    .attr("r", 5)
+                    .style("fill", "#69b3a2");
 
-     // Draw dots for 2020
-    svg.selectAll(".dot2020")
-        .data(expenses2020)
-        .enter()
-        .append("circle")
-        .attr("class", "dot2020")
-        .attr("cx", d => x(d.x1))
-        .attr("cy", d => y(d.Purpose) + y.bandwidth() / 2)
-        .attr("r", 5)
-        .style("fill", "#69b3a2")
+                // Draw dots for 2022
+                svg.selectAll(".dot2022")
+                    .data(expensesData)
+                    .enter()
+                    .append("circle")
+                    .attr("class", "dot2022")
+                    .attr("cx", d => xScale(d.x2)) // Use x2 for 2022
+                    .attr("cy", d => yScale(d.Purpose) + yScale.bandwidth() / 2)
+                    .attr("r", 5)
+                    .style("fill", "#4C4082");
 
-      // Draw dots for 2022
-      svg.selectAll(".dot2022")
-        .data(expenses2022)
-        .enter()
-        .append("circle")
-        .attr("class", "dot2022")
-        .attr("cx", d => x(d.x2))
-        .attr("cy", d => y(d.Purpose) + y.bandwidth() / 2)
-        .attr("r", 5)
-        .style("fill", "#4C4082")
 
-      // Draw x-axis
-      svg.append("g")
-        .attr("transform", `translate(0,${height})`)
-        .call(d3.axisBottom(x));
+                // Draw x-axis
+                const xAxis = d3.axisBottom(xScale);
 
-      // Draw y-axis
-      svg.append("g")
-        .call(d3.axisLeft(y));
-    
-    // Draw legend  
-    const legend = svg.append("g")
-    .attr("class", "legend")
-    .attr("transform", `translate(${width + 20}, 0)`);
-    
-    legend.append("circle")
-        .attr("cx", 0)
-        .attr("cy", 0)
-        .attr("r", 5)
-        .style("fill", "#69b3a2");
+                svg.append("g")
+                    .attr("transform", `translate(0,${height})`)
+                    .call(xAxis)
+                  .append("text")
+                    .attr("fill", "#000")
+                    .attr("x", width)
+                    .attr("dy", "-0.71em")
+                    .attr("text-anchor", "end")
 
-    legend.append("text")
-        .attr("x", 10)
-        .attr("y", 5)
-        .text("2020");
 
-    legend.append("circle")
-        .attr("cx", 0)
-        .attr("cy", 20)
-        .attr("r", 5)
-        .style("fill", "#4C4082");
 
-    legend.append("text")
-        .attr("x", 10)
-        .attr("y", 25)
+                const yAxis = d3.axisLeft(yScale);
 
-       .text("2022");
-    }
-// Function to handle updates based on dropdown changes
-function updateCharts() {
-    // Filter data based on selections
-    const selectedOriginCountry = d3.select("#Origin_Country_Name").node().value;
-    const selectedDestinationCountry = d3.select("#Destination_Country_Name").node().value;
+                    svg.append("g")
+                        .call(yAxis)
+                      .append("text")
+                        .attr("fill", "#000")
+                        .attr("transform", "rotate(-90)")
+                        .attr("y", 6)
+                        .attr("dy", "0.71em")
+                        .attr("text-anchor", "end")
 
-    let filteredData = data;
-    if (selectedOriginCountry !== "All") {
-        filteredData = filteredData.filter(d => d.Origin_Country_Name === selectedOriginCountry);
-    }
-    if (selectedDestinationCountry !== "All") {
-        filteredData = filteredData.filter(d => d.Destination_Country_Name === selectedDestinationCountry);
-    }
-    // Call function to create visualizations
-    createLineChart(filteredData);
-}
+                // Draw legend  
+                const legend = svg.append("g")
+                .attr("class", "legend")
+                .attr("transform", `translate(${width + 20}, 0)`);
 
-// Initially render the chart
-updateCharts();
+            legend.append("circle")
+                .attr("cx", 0)
+                .attr("cy", 0)
+                .attr("r", 5)
+                .style("fill", "#69b3a2");
+       
 
-});
+            legend.append("text")
+                .attr("x", 10)
+                .attr("y", 5)
+                .text("2019");
 
+            legend.append("circle")
+                .attr("cx", 0)
+                .attr("cy", 20)
+                .attr("r", 5)
+                .style("fill", "#4C4082");
+               
+
+            legend.append("text")
+                .attr("x", 10)
+                .attr("y", 25)
+                .text("2022");
+            }
+
+            // Function to handle updates based on dropdown changes
+            function updateChart() {
+                const selectedOriginCountry = d3.select("#Origin_Country").node().value;
+                const selectedDestinationCountry = d3.select("#Destination_Country").node().value;
+                createChart(selectedOriginCountry, selectedDestinationCountry);
+            }
+
+            // Update the chart when a change takes place to the dropdown
+            d3.select("#Origin_Country").on("change", updateChart);
+            d3.select("#Destination_Country").on("change", updateChart);
+
+            // Initial population of dropdowns
+            const originDropdown = d3.select("#Origin_Country");
+            const destinationDropdown = d3.select("#Destination_Country");
+
+            originDropdown.selectAll("option")
+                .data(uniqueCountries)
+                .enter()
+                .append("option")
+                .attr("value", d => d)
+                .text(d => d);
+
+            destinationDropdown.selectAll("option")
+                .data(uniqueCountries)
+                .enter()
+                .append("option")
+                .attr("value", d => d)
+                .text(d => d);
+        });
--- test.js
+++ test.js
@@ -1,57 +1,177 @@
 // Load data from the CSV file
-d3.csv("data/tourism_exp_dest.csv").then(data => {
-    data = data.filter(d => (d.Year === "2019" || d.Year === "2022"));
+d3.csv("./data/tourism_exp_dest.csv").then(data => {
+  // Parse data
+  data.forEach(d => {
+      d.Year = +d.year;
+      d.Origin_Country_Code = d.Origin_Country_Code;
+      d.Origin_Country_Name = d.Origin_Country;
+      d.Destination_Country_Code = d.Destination_Country_Code;
+      d.Destination_Country_Name = d.Destination_Country;
+      d.Purpose = d.Purpose;
+      d.Total_Expenditure_EUR = +d.Total_Expenditure_EUR;
+  });
 
-    // Other setup and filtering logic remain the same
+  // Filter data based on selections
+  const uniqueCountries = Array.from(new Set(data.map(d => d.Destination_Country_Name)));
 
-    // ... (Previous code for dropdowns and filters)
+  // Function to calculate expenses for 2020 and 2022
+  function calculateExpenses(selectedOriginCountry, selectedDestinationCountry) {
+      const filteredData = data.filter(d =>
+          d.Origin_Country_Name === selectedOriginCountry &&
+          d.Destination_Country_Name === selectedDestinationCountry &&
+          (d.Year === 2019 || d.Year === 2022)
+      );
 
-    // Update the charts when a change takes place to the DOM
-    d3.selectAll("#Origin_Country_Name, #Destination_Country_Name, #Purpose, #Total_Expenditure_EUR, #Year")
-        .on("change", updateCharts);
+      const expenses2020 = filteredData.filter(d => d.Year === 2019).reduce((acc, cur) => {
+          acc[cur.Purpose] = acc[cur.Purpose] ? acc[cur.Purpose] + cur.Total_Expenditure_EUR : cur.Total_Expenditure_EUR;
+          return acc;
+      }, {});
 
-    // Calculate expenses for 2020 and 2022 within this function
-    const expenses2020 = data.filter(d => d.Year === "2019" || d.Year === "2020");
-    const expenses2022 = data.filter(d => d.Year === "2021" || d.Year === "2022");
+      const expenses2022 = filteredData.filter(d => d.Year === 2022).reduce((acc, cur) => {
+          acc[cur.Purpose] = acc[cur.Purpose] ? acc[cur.Purpose] + cur.Total_Expenditure_EUR : cur.Total_Expenditure_EUR;
+          return acc;
+      }, {});
 
-    const mergedExpenses = expenses2020.map(expense => ({
-        Purpose: expense.Purpose,
-        x1: +expenses2020.find(e => e.Purpose === expense.Purpose)?.Total_Expenditure_EUR || 0,
-        x2: +expenses2022.find(e => e.Purpose === expense.Purpose)?.Total_Expenditure_EUR || 0
-    }));
+      return Object.keys(expenses2020).map(purpose => ({
+          Purpose: purpose,
+          x1: expenses2020[purpose] || 0,
+          x2: expenses2022[purpose] || 0
+      }));
+  }
 
-    console.log(mergedExpenses);
+  // Create initial chart
+  createChart(uniqueCountries[0], uniqueCountries[1]); // Default country selection
 
-    // Create a line chart
-    function createLineChart(data) {
-        // Rest of your chart creation logic remains the same
-    }
+  // Function to handle updates based on dropdown changes
+  function updateChart() {
+      const selectedOriginCountry = d3.select("#Origin_Country").node().value;
+      const selectedDestinationCountry = d3.select("#Destination_Country").node().value;
+      createChart(selectedOriginCountry, selectedDestinationCountry);
+  }
 
-    // Function to handle updates based on dropdown changes
-    function updateCharts() {
-        // Filter data based on selections
-        const selectedOriginCountry = d3.select("#Origin_Country_Name").node().value;
-        const selectedDestinationCountry = d3.select("#Destination_Country_Name").node().value;
-        const selectedPurpose = d3.select("#Purpose").node().value;
-        const selectedYear = d3.select("#Year").node().value;
+  // Function to create the Cleveland dot plot
+  function createChart(selectedOriginCountry, selectedDestinationCountry) {
+      const expensesData = calculateExpenses(selectedOriginCountry, selectedDestinationCountry);
+      console.log(expensesData);
 
-        let filteredData = data;
-        if (selectedOriginCountry !== "All") {
-            filteredData = filteredData.filter(d => d.Origin_Country_Name === selectedOriginCountry);
-        }
-        if (selectedDestinationCountry !== "All") {
-            filteredData = filteredData.filter(d => d.Destination_Country_Name === selectedDestinationCountry);
-        }
-        if (selectedPurpose !== "All") {
-            filteredData = filteredData.filter(d => d.Purpose === selectedPurpose);
-        }
-        if (selectedYear !== "All") {
-            filteredData = filteredData.filter(d => d.Year === selectedYear);
-        }
-        // Call function to create visualizations
-        createLineChart(filteredData);
-    }
+      // Dimensions and margins for the chart
+      const margin = { top: 30, right: 250, bottom: 30, left: 120 };
+      const width = 1000 - margin.left - margin.right;
+      const height = 400 - margin.top - margin.bottom;
 
-    // Initially render the chart
-    updateCharts();
+      // Create SVG container
+      const svg = d3.select("#chart")
+          .selectAll("*")
+          .remove()
+          .append("svg")
+          .attr("width", width + margin.left + margin.right)
+          .attr("height", height + margin.top + margin.bottom)
+          .append("g")
+          .attr("transform", `translate(${margin.left},${margin.top})`);
+
+      // Chart creation logic here using expensesData
+      // Draw lines
+      svg.selectAll(".line")
+          .data(expensesData)
+          .enter()
+          .append("line")
+          .attr("class", "line")
+          .attr("x1", d => xScale(d.x1))
+          .attr("y1", d => yScale(d.Purpose) + yScale.bandwidth() / 2)
+          .attr("y2", d => yScale(d.Purpose) + yScale.bandwidth() / 2)
+          .attr("x1", d => xScale(d.x2))
+          .attr("stroke", "GREY")
+          .attr("stroke-width", 1);
+
+      // Draw dots for 2020
+      svg.selectAll(".dot2020")
+          .data(expensesData)
+          .enter()
+          .append("circle")
+          .attr("class", "dot2020")
+          .attr("cx", d => xScale(d.x1))
+          .attr("cy", d => yScale(d.Purpose) + yScale.bandwidth() / 2)
+          .attr("r", 5)
+          .style("fill", "#69b3a2");
+
+      // Draw dots for 2022
+      svg.selectAll(".dot2022")
+          .data(expensesData)
+          .enter()
+          .append("circle")
+          .attr("class", "dot2022")
+          .attr("cx", d => xScale(d.x1))
+          .attr("cy", d => yScale(d.Purpose) + yScale.bandwidth() / 2)
+          .attr("r", 5)
+          .style("fill", "#4C4082");
+
+      // X axis scale
+      const xScale = d3.scaleLinear()
+          .domain([0, d3.max(expensesData, d => Math.max(d.x1, d.x2))])
+          .range([0, width]);
+
+      // Draw x-axis
+      svg.append("g")
+          .attr("transform", `translate(0,${height})`)
+          .call(d3.axisBottom(xScale));
+
+      // Y axis scale
+      const yScale = d3.scaleBand()
+          .domain(expensesData.map(d => d.Purpose))
+          .range([0, height])
+          .padding(0.1);
+          
+      // Draw y-axis
+      svg.append("g")
+          .call(d3.axisLeft(yScale));
+
+      // Draw legend  
+      const legend = svg.append("g")
+          .attr("class", "legend")
+          .attr("transform", `translate(${width + 20}, 0)`);
+
+      legend.append("circle")
+          .attr("cx", 0)
+          .attr("cy", 0)
+          .attr("r", 5)
+          .style("fill", "#69b3a2");
+
+      legend.append("text")
+          .attr("x", 10)
+          .attr("y", 5)
+          .text("2020");
+
+      legend.append("circle")
+          .attr("cx", 0)
+          .attr("cy", 20)
+          .attr("r", 5)
+          .style("fill", "#4C4082");
+
+      legend.append("text")
+          .attr("x", 10)
+          .attr("y", 25)
+          .text("2022");
+  }
+
+  // Update the chart when a change takes place to the dropdown
+  d3.select("#Origin_Country").on("change", updateChart);
+  d3.select("#Destination_Country").on("change", updateChart);
+
+  // Initial population of dropdowns
+  const originDropdown = d3.select("#Origin_Country");
+  const destinationDropdown = d3.select("#Destination_Country");
+
+  originDropdown.selectAll("option")
+      .data(uniqueCountries)
+      .enter()
+      .append("option")
+      .attr("value", d => d)
+      .text(d => d);
+
+  destinationDropdown.selectAll("option")
+      .data(uniqueCountries)
+      .enter()
+      .append("option")
+      .attr("value", d => d)
+      .text(d => d);
 });
